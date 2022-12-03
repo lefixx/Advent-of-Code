95a04e07c5d3864fe64027a9e21a6454b1db7c8a
lefixx <lefixx@gmail.com>
2022-12-03 19:39:50 +0200

    reorder

--- 2022/Day 1/day1.lua
+++ 2022/Day 1/day1.lua
@@ -1,1 +1,16 @@
-print("hello")
+local caloricCount = 0
+local caloriesPerSack = {}
+
+
+for line in io.lines("E:/[Code]/AdventofCode/Advent-of-Code/2022/Day 1/input.txt") do
+    if line == "" then 
+        table.insert(caloriesPerSack,caloricCount)
+        caloricCount = 0
+    else 
+        caloricCount = caloricCount + line
+    end
+end
+
+table.sort(caloriesPerSack, function(a,b) return a > b end)
+print(caloriesPerSack[1])
+print(caloriesPerSack[1]+caloriesPerSack[2]+caloriesPerSack[3])
