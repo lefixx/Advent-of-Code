38109a0fe4ed7e1497915a20ae8c3a346989145b
lefixx <lefixx@gmail.com>
2023-12-27 11:02:00 +0200

    added comments

--- 2023/day1/code.lua
+++ 2023/day1/code.lua
@@ -1,4 +1,4 @@
-function lines_from(file) ---- get all lines from a file
+function lines_from(file) ---- get all lines from a file in the form of a table
     local lines = {}
     for line in io.lines(file) do
         table.insert(lines, line)
@@ -10,8 +10,8 @@ local input = lines_from("puzzleinput")
 
 local sum = 0
 for k,line in pairs(input) do
-    local firstNumber = string.match(line, "%d")
-    local lastNumber = string.match(string.reverse(line), "%d")
+    local firstNumber = string.match(line, "%d") -- using pattern matching to get the first digit
+    local lastNumber = string.match(string.reverse(line), "%d") -- reversing the string before searching returns the last digit
     sum = sum + (firstNumber..lastNumber)
 end
 print(sum)
