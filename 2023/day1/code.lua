a26a9fec4338b93145c0bc617a08f4dae3f9c278
lefixx <lefixx@gmail.com>
2023-12-27 10:46:30 +0200

    change to use table.insert

--- 2023/day1/code.lua
+++ 2023/day1/code.lua
@@ -1,8 +1,13 @@
-function lines_from(file) ---- get all lines from a file, returns nil if the file does not exist
-    if not file_exists(file) then return nil end
+function lines_from(file) ---- get all lines from a file
     local lines = {}
     for line in io.lines(file) do
-        lines[#line+1] = line
+        table.insert(lines, line)
     end
     return lines
 end
+
+-- local foo = io.open("puzzleinput","r")
+local input = lines_from("puzzleinput")
+
+
+print(input[2])
