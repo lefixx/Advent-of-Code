c7a66b333b0ac17f934a3db329d9482b79c58ec7
lefixx <lefixx@gmail.com>
2023-12-30 14:46:04 +0200

    made a helper function, make reverse lookuptable

--- 2023/day1/code.lua
+++ 2023/day1/code.lua
@@ -6,6 +6,12 @@ function lines_from(file) ---- get all lines from a file in the form of a table
     return lines
 end
 
+
+function printTable(table)
+    for k,v in pairs(table) do
+        print(k,v)
+    end
+end
 local input = lines_from("puzzleinput")
 
 local sum = 0
@@ -16,31 +22,17 @@ local sum = 0
 -- end
 
 
-for k,line in pairs(input) do
+local allPatterns = { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine","1","2","3","4","5","6","7","8","9","0" }
+local pattern = { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"}
 
-    local foo = ""
-    foo = string.find(line, "one")
-    print(line, string.match(line, "(one)(two)(three)(four)(five)(six)(seven)(eight)(nine)"))
-    print(line, string.match(line, "(one)(two)"))
-
-
-
-
-
-    line = string.gsub(line,"one","1")
-    line = string.gsub(line,"two","2")
-    line = string.gsub(line,"three","3")
-    line = string.gsub(line,"four","4")
-    line = string.gsub(line,"five","5")
-    line = string.gsub(line,"six","6")
-    line = string.gsub(line,"seven","7")
-    line = string.gsub(line,"eight","8")
-    line = string.gsub(line,"nine","9")
-    local firstNumber = string.match(line, "%d") -- using pattern matching to get the first digit
-    local lastNumber = string.match(string.reverse(line), "%d") -- reversing the string before searching returns the last digit
-    -- print(line,firstNumber, lastNumber, "\n")
-    sum = sum + (firstNumber..lastNumber)
+reversePatterns = {}
+for k,v in ipairs(allPatterns) do
+    reversePatterns[k] = string.reverse(v)
 end
+
+
+
+
 --108239 too high
 --53789 too low
 print(sum)
